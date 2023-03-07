507e2ab5fec7b7819b31db77e789434e91aa22c5
Dhan Bir Sunwar <deanvr42@gmail.com>
2023-03-07 12:00:48 +0600

    initial

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Box, Stack, Typography, Divider } from '@mui/material';
+import Logo from '../assets/images/Logo.png';
+
+const Footer = () => (
+  <Box mt="80px">
+     <Divider light />
+    <Stack gap="40px" sx={{ alignItems: 'center' }} flexWrap="wrap" px="40px" py="24px">
+      <img src={Logo} alt="logo" style={{ width: '80px', height: '41px' }} />
+      <Typography>
+      © {new Date().getFullYear()} Gym pro. All rights reserved.
+      </Typography>
+    </Stack>
+  </Box>
+);
+
+export default Footer;
---
+++ src/utils/fetchData.js
@@ -0,0 +1,22 @@
+export const exerciseOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key': 'ef51910368mshc470d7c8577771fp161816jsn2cf45e543ae0',
+  },
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': 'ef51910368mshc470d7c8577771fp161816jsn2cf45e543ae0',
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
